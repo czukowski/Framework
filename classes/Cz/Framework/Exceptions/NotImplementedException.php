c4e611ef3cabbb4b51f9d78d76aad4229aed0754
Korney Czukowski <carbofos@seznam.cz>
2014-03-19 08:01:08 +0100

    Added new exception type

---
+++ classes/Cz/Framework/Exceptions/NotImplementedException.php
@@ -0,0 +1,14 @@
+<?php
+namespace Cz\Framework\Exceptions;
+
+/**
+ * NotSupportedException
+ * 
+ * @package    Framework
+ * @category   Exceptions
+ * @author     Korney Czukowski
+ * @copyright  (c) 2014 Korney Czukowski
+ * @license    MIT License
+ */
+class NotImplementedException extends \Exception implements Exception
+{}
