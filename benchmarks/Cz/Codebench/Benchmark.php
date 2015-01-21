c9aa08590f133ce7d0d580f3e91d8e86e8e4c073
Korney Czukowski <carbofos@seznam.cz>
2015-01-21 09:41:38 +0100

    Own Benchmark class extends Codebench for convenience

---
+++ benchmarks/Cz/Codebench/Benchmark.php
@@ -0,0 +1,14 @@
+<?php
+namespace Cz\Codebench;
+
+/**
+ * Convenience class that extends from original Codench class.
+ * 
+ * @package    Framework
+ * @category   Callbacks
+ * @author     Korney Czukowski
+ * @copyright  (c) 2015 Korney Czukowski
+ * @license    MIT License
+ */
+class Benchmark extends \Codebench
+{}
